56ca1c8236e7849f68e1512b1af1f7e0d1bb5d2a
Ojas-Shukla <ojasshukla2007@gmail.com>
2025-12-09 11:33:09 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/12ZBe-y0oGkn0kWymozNAYGVermQznDzW
 """
 
-#pip install streamlit scikit-learn pandas numpy joblib
+pip install streamlit scikit-learn pandas numpy joblib
 
 import streamlit as st
 import pandas as pd
